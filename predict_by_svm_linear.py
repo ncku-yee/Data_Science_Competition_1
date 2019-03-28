75b0943e39e0e71fe799fe2e8f0d4cf508423e7b
吳添毅 <asd887722456@gmail.com>
2019-03-28 10:10:13 +0800

    acc=0.8595

--- predict_by_svm_linear.py
+++ predict_by_svm_linear.py
@@ -14,28 +14,38 @@ from sklearn.model_selection import train_test_split
 df_train = pd.read_csv('train.csv')
 df_test = pd.read_csv('test.csv')
 print(df_train.columns)
-x = df_train[['CreditScore', 'Age', 'Balance', 'Tenure', 'HasCrCard', 'IsActiveMember', 'EstimatedSalary']].values
-
+x = df_train[['CreditScore', 'Geography', 'Gender', 'Age', 'Balance', 'Tenure', 'NumOfProducts',  'HasCrCard', 'IsActiveMember', 'EstimatedSalary']].copy()
+x['Geography'] = x['Geography'].replace(['S0', 'S1', 'S2'], [0, 1, 2])
+x['Gender'] = x['Gender'].replace(['Male', 'Female'], [0, 1])
+x = x.values
 y = df_train['Exited'].values
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.1, random_state = 500, stratify=y)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 0, stratify=y)
 
 # =============================================================================
-# x_train = df_train[['Tenure', 'HasCrCard', 'NumOfProducts', 'IsActiveMember']].values
+# x_train = df_train[['CreditScore', 'Geography', 'Gender', 'Age', 'Balance', 'Tenure', 'NumOfProducts',  'HasCrCard', 'IsActiveMember', 'EstimatedSalary']]
+# x_train['Geography'] = x_train['Geography'].replace(['S0', 'S1', 'S2'], [0, 1, 2])
+# x_train['Gender'] = x_train['Gender'].replace(['Male', 'Female'], [0, 1])
+# x_train = x_train.values
 # 
 # y_train = df_train['Exited'].values
 # 
-# x_test = df_test[['Tenure', 'HasCrCard', 'NumOfProducts', 'IsActiveMember']].values
+# x_test = df_test[['CreditScore', 'Geography', 'Gender', 'Age', 'Balance', 'Tenure', 'NumOfProducts',  'HasCrCard', 'IsActiveMember', 'EstimatedSalary']]
+# x_test['Geography'] = x_test['Geography'].replace(['S0', 'S1', 'S2'], [0, 1, 2])
+# x_test['Gender'] = x_test['Gender'].replace(['Male', 'Female'], [0, 1])
+# x_test = x_test.values
 # =============================================================================
 #%%
 from sklearn import svm
 
 #clf = svm.SVC(kernel='linear')
-#clf = svm.SVC(kernel='rbf')
+clf = svm.SVC(kernel='rbf', gamma=0.01)
 
-clf = svm.SVC(gamma = 1, decision_function_shape = 'ovo')
-#y_predict = clf.predict(x_test)
+#clf = svm.SVC(gamma = 0.01, decision_function_shape = 'ovo')
 
 clf.fit(x_train, y_train)
+
+#y_predict = clf.predict(x_test)
+
 acc = clf.score(x_test, y_test)
 
 
@@ -56,4 +66,4 @@ acc = accuracy_score(yt, y_test)
 #%%
 df = pd.DataFrame({'RowNumber': df_test['RowNumber'], 'Exited': list(y_predict)})
 
-str_csv = df.to_csv(r'./predict_exited_3.csv',columns=['RowNumber','Exited'],index=True,sep=',')
+str_csv = df.to_csv(r'./predict_exited_4.csv',columns=['RowNumber','Exited'],index=True,sep=',')
